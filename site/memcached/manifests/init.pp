90353b186669416e852c1d3a9c4857a97f0a01cd
cbrown7418 <cbrown7418@comcast.net>
2016-04-26 16:57:40 -0500

    create memcached service and enable

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,25 @@
+#
+class memcached{
+#PACKAGE
+package{'memcached':
+ensure => present,
+}
+
+# FILE 
+file { '/etc/sysconfig/memcached':
+ ensure => file,
+ owner => 'root',
+ group => 'root',
+ mode  => '0644',
+ source => 'puppet:///modules/memcached/memcached
+ require => 'Package['memcached']
+}
+
+#SERVICE
+service{memcached:
+ ensure => running,
+ enabled => true,
+ subscribe => ['/etc/sysconfig/memchached'],
+}
+
+}
