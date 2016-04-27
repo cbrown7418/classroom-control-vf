711c761cd5af58aa6f8b8d3431bd07966352277e
cbrown7418 <cbrown7418@comcast.net>
2016-04-27 13:13:25 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,7 +44,8 @@ node default {
 # class { 'my_class': }
  if $::virtual != 'physical' {
  $vmname = capitalize($::virtual)
- notify { "This is a ${vmname} virtual machine.": }
+ notify { "This is a ${vmname} virtual machine!!.": }
  include nginx
+ include memcached
  }
 }
